348aa5146b0f64f136dadcf31c391d3b2916c90a
Bogdan <b.ivko@sysiq.com>
2013-09-26 10:47:01 +0300

    Contacts form functionality

---
+++ main.php
@@ -0,0 +1,49 @@
+<?php
+	$contactsFile = "res/js/contacts.json";
+	$groupsFile = "res/js/groups.json";
+	$string = file_get_contents($contactsFile);
+	$json = json_decode($string, true);
+		
+	foreach ($json as $key => $val) {
+		if(is_array($val)) {
+			/*Поиск макс ID*/
+			$lastID = 0;
+			foreach ($val as $z => $y) {
+				if(!is_array($y) && $z=="userID" && $lastID < $y) $lastID= $y+1;
+			}
+		}
+	}
+	
+	if($_GET['type'] == 'editContact'){
+		if($_GET['userID']=='new'){
+			/*добавляем запись*/
+			$json['user'.$lastID]=[
+				'userID' => $lastID,
+				'name' => $_GET['name'],
+				'mail' => $_GET['mail'],
+				'phone' => $_GET['phone'],
+				'group' => $_GET['group']
+			];
+			echo json_encode(['status' => 'add','userID' => $lastID]);
+		}else{
+			/*обновляем запись*/
+			$json['user'.$_GET['userID']]=[
+				'userID' => $_GET['userID'],
+				'name' => $_GET['name'],
+				'mail' => $_GET['mail'],
+				'phone' => $_GET['phone'],
+				'group' => $_GET['group']
+			];
+			echo '';
+			echo json_encode(['status' => 'update']);
+		}
+	}else if($_GET['type'] == 'removeContact'){
+		unset($json['user'.$_GET['userID']]);
+		echo 'delete';
+	}else{
+		echo 'error request type';
+	};
+	/*Запись*/
+	$string = json_encode($json);	
+	file_put_contents($contactsFile, $string);
+?>
